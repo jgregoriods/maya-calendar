f7affd2120c46ebdb787dab9d45859516e08ddad
5ManikG7 <jonas.gregorio@gmail.com>
2018-08-25 13:32:35 -0300

    Change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/js/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist/js'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js'
   },
 };
