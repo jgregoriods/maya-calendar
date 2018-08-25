117efcfc32e6a054a70774b3ea7768ecf502c0a5
5ManikG7 <jonas.gregorio@gmail.com>
2018-08-25 12:21:12 -0300

    Add webpack bundle

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,6 @@
+import MayaDate from './models/MayaDate.js';
+import * as convert from './actions/convert.js';
+
 /**
  * MAYA LONG COUNT: Tools for converting between Maya and Western dates
  * Author: Jonas Gregorio de Souza
@@ -5,189 +8,6 @@
  */
 
 const dateController = (function() {
-  const tzolkinNames = ['Imix', 'Ik', 'Akbal', 'Kan', 'Chicchan', 'Cimi', 'Manik', 'Lamat', 'Muluc', 'Oc', 'Chuen', 'Eb', 'Ben', 'Ix', 'Men', 'Cib', 'Caban', 'Edznab', 'Cauac', 'Ahau'];
-
-  const haabNames = ['Pop', 'Uo', 'Zip', 'Zotz', 'Tzec', 'Xul', 'Yaxkin', 'Mol', 'Chen', 'Yax', 'Zac', 'Ceh', 'Mac', 'Kankin', 'Muan', 'Pax', 'Kayab', 'Cumku', 'Uayeb'];
-
-  class MayaDate {
-    constructor(longCount) {
-      this.longCount = longCount;
-    }
-  
-    getLongCount() {
-      return this.longCount;
-    }
-
-    setTzolkin() {
-      const numberOfDays = convertToDecimal(this.longCount);
-  
-      // An offset of 4 to account for the base date of 4 Ahau
-      let tzolkinNumber = (numberOfDays + 4) % 13;
-      if (tzolkinNumber === 0) tzolkinNumber = 13;
-  
-      // An offset of 19 to account for the base date of 4 Ahau
-      const tzolkinName = tzolkinNames[(numberOfDays + 19) % 20];
-  
-      this.tzolkin = [tzolkinNumber, tzolkinName];
-    }
-
-    getTzolkin() {
-      return this.tzolkin;
-    }
-  
-    setHaab() {
-      const numberOfDays = convertToDecimal(this.longCount);
-  
-      // An offset of 348 is due to the base date of 8 Cumku
-      const dayOfYear = (numberOfDays + 348) % 365;
-      let haabNumber;
-      let haabName;
-
-      if (dayOfYear < 361) {
-        haabNumber = (dayOfYear % 20);
-        haabName = haabNames[Math.floor(dayOfYear / 20)];
-      } else {
-        haabNumber = dayOfYear - 360;
-        haabName = 'Uayeb';
-      }
-
-      this.haab = [haabNumber, haabName];
-    }
-  
-    getHaab() {
-      return this.haab;
-    }
-
-    setLordOfNight() {
-      const numberOfDays = convertToDecimal(this.longCount);
-      if (numberOfDays % 9 === 0) {
-        this.lordOfNight = 9;
-      } else {
-      this.lordOfNight = numberOfDays % 9;
-      }
-    }
-
-    getLordOfNight() {
-      return this.lordOfNight;
-    }
-  }
-
-  // Conversions between numeric systems
-
-  /**
-   * Converts a vigesimal number in the Maya system to its decimal equivalent.
-   * 
-   * @param {Array} vigesimalNumber The vigesimal Maya number to be converted. Must be an array of length 5.
-   * @returns {number} A number in the decimal system.
-   */
-  function convertToDecimal(vigesimalNumber) {
-    const vigesimalPlaces = [144000, 7200, 360, 20, 1];
-    let decimalNumber = 0;
-  
-    for (let i = 0; i < vigesimalPlaces.length; i++) {
-      decimalNumber += vigesimalNumber[i] * vigesimalPlaces[i];
-    }
-  
-    return decimalNumber;
-  }
-
-  /**
-   * Converts a decimal number to its equivalent in the Maya vigesimal system.
-   * 
-   * @param {number} decimalNumber The number to be converted.
-   * @returns {Array} A Maya vigesimal number as an array of length 5.
-   */
-  function convertToVigesimal(decimalNumber) {
-    const vigesimalPlaces = [144000, 7200, 360, 20, 1];
-    const vigesimalNumber = [];
-  
-    let remainder = decimalNumber;
-    for (let i = 0; i < vigesimalPlaces.length; i++) {
-      vigesimalNumber.push(Math.floor(remainder / vigesimalPlaces[i]));
-      remainder %= vigesimalPlaces[i];
-    }
-  
-    return vigesimalNumber;
-  }
-
-  // Conversions between calendar systems
-
-  /**
-   * Converts a Maya date to the Gregorian calendar.
-   * 
-   * @param {Object} mayaDate The Maya date to be converted. Must be an object of class MayaDate.
-   * @param {number} constant The correlation constant to perform the conversion.
-   * @returns {Date} A date object.
-   */
-  function convertMayaToGreg(mayaDate, constant) {
-    const daysFromJulianZero = convertToDecimal(mayaDate.getLongCount()) + constant;
-  
-    // Initially set the Gregorian Date to Julian Day Number Zero
-    const gregorianDate = new Date (-4713, 10, 24);
-    gregorianDate.setDate(gregorianDate.getDate() + daysFromJulianZero);
-  
-    return gregorianDate;
-  }
-
-  /**
-   * Converts a Gregorian date to the Maya calendar.
-   * 
-   * @param {Date} gregorianDate The date to be converted. Must be a Date object.
-   * @param {number} constant The correlation constant to perform the conversion.
-   * @returns {Object} An object of class MayaDate.
-   */
-  function convertToMaya(gregorianDate, constant) {
-    const julianZero = new Date (-4713, 10, 24);
-    const daysFromJulianZero = Math.round((gregorianDate - julianZero) / 86400000);
-    const daysFromMayaZero = daysFromJulianZero - constant;
-
-    return new MayaDate(convertToVigesimal(daysFromMayaZero));
-  }
-
-  /**
-   * Converts a Gregorian date to the Julian calendar.
-   * 
-   * @param {Date} gregorianDate The date to be converted. Must be a Date object.
-   * @returns {Date} A Date object representing a date in the Julian calendar.
-   */
-  function convertToJulian(gregorianDate) {
-    // Last alignment between Gregorian and Julian calendars
-    const calendarBase = new Date(200, 2, 1);
-
-    const daysFromBase = (gregorianDate - calendarBase) / 86400000;
-    const centuriesFromBase = Math.floor(daysFromBase / 36524);
-    const extraLeapYears = Math.floor((centuriesFromBase + 2) / 4);
-    const daysToShift = centuriesFromBase - extraLeapYears;
-  
-    const julianDate = new Date(gregorianDate.getFullYear(), gregorianDate.getMonth(),
-      gregorianDate.getDate());
-    julianDate.setDate(gregorianDate.getDate() - daysToShift);
-
-    return julianDate;
-  }
-
-  /**
-   * Converts a Julian date to the Gregorian calendar.
-   * 
-   * @param {Date} julianDate The date to be converted. Must be a Date object.
-   * @returns {Date} A Date object representing a date in the Gregorian calendar.
-   */
-  function convertJulianToGreg(julianDate) {
-    // Last alignment between Gregorian and Julian calendars
-    const calendarBase = new Date(200, 2, 1);
-
-    const daysFromBase = (julianDate - calendarBase) / 86400000;
-    const centuriesFromBase = Math.floor(daysFromBase / 36524);
-    const extraLeapYears = Math.floor((centuriesFromBase + 2) / 4);
-    const daysToShift = centuriesFromBase - extraLeapYears;
-
-    const gregorianDate = new Date(julianDate.getFullYear(), julianDate.getMonth(),
-    julianDate.getDate());
-    gregorianDate.setDate(julianDate.getDate() + daysToShift);
-
-    return gregorianDate;
-  }
-
   return {
     addMayaDate: function(longCount) {
       const newMayaDate = new MayaDate(longCount);
@@ -200,11 +20,11 @@ const dateController = (function() {
     },
 
     getGregFromMaya: function(mayaDate, constant) {
-      return convertMayaToGreg(mayaDate, constant);
+      return convert.mayaToGreg(mayaDate, constant);
     },
 
     getMayaDate: function(gregorianDate, constant) {
-      const mayaDate = convertToMaya(gregorianDate, constant);
+      const mayaDate = convert.toMaya(gregorianDate, constant);
 
       mayaDate.setTzolkin();
       mayaDate.setHaab();
@@ -214,11 +34,11 @@ const dateController = (function() {
     },
 
     getJulianFromGreg: function(gregorianDate) {
-      return convertToJulian(gregorianDate);
+      return convert.toJulian(gregorianDate);
     },
 
     getGregFromJulian: function(julianDate) {
-      return convertJulianToGreg(julianDate);
+      return convert.julianToGreg(julianDate);
     }
   }
 })();
